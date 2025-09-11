c7b4a69c3fc2611c1a38cdd77f843cbdcbf7ceae
Maria <mariaclara.inaciocosta@gmail.com>
2025-09-11 10:44:38 -0300

    Codigos da aula 06 e 07

--- aulas/aula06/index.js
+++ aulas/aula06/index.js
@@ -1,15 +1,42 @@
 // 1. importar o framework
 const express = require("express");
+// importar middleware de terceiros
+const cors = require('cors');
 
-// 2.criar uma instância da aplicção
+const router = require('./router');
+
+// 2. criar uma instância da aplicação
 const app = express();
 
-// Criar um middleware
-app.get('/', (req,res) => {
+// middleware embutido ou integrado
+app.use(express.json());
+//?param1=valor1&param2=valor2...
+app.use(express.urlencoded({ extended: false }));
+
+// middleware de terceiros
+app.use(cors());
+
+// middleware de aplicação
+app.use((req, res, next) => {
+  console.log("Passei pelo middleware de app");
+  next();
+});
+
+
+// registra o router no caminho /tarefas
+app.use('/tarefas', router);
+
+// rota simples na raiz
+app.get('/', (req, res) => {
   res.send("Olá!");
 });
 
+// middleware de erro
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+});
+
 // 3. iniciar a aplicação em uma porta
-app.listen(3000, ()=>{
-    console.log("App está On!");
-})
+app.listen(3000, () => {
+  console.log("App está On!");
+});
